38214865bb4b01a372bb8c5780fc25acd0c22f49
pwoogi <chrishw1029@gmail.com>
2022-11-02 01:13:24 +0900

    GetMapping 3가지 방법으로 전송해보기 1. Map 2. @RequestParam 3. DTO

--- src/main/java/com/example/hello/controller/GetApiController.java
+++ src/main/java/com/example/hello/controller/GetApiController.java
@@ -1,6 +1,9 @@
 package com.example.hello.controller;
 
+import com.example.hello.dto.UserRequest;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/get")
@@ -22,5 +25,42 @@ public class GetApiController {
         return pathName;
     }
 
+    @GetMapping(path = "query-param")
+    public String queryParam(@RequestParam Map<String, String> queryParam){
 
+        StringBuilder sb = new StringBuilder();
+        queryParam.entrySet().forEach(entry-> {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+            System.out.println("\n");
+
+            sb.append(entry.getKey() + " = " + entry.getValue() +"\n");
+
+        });
+
+        return sb.toString();
+    }
+
+    @GetMapping(path = "query-param02")
+    public String queryParam02(
+            @RequestParam String name,
+            @RequestParam String email,
+            @RequestParam int age){
+
+        System.out.println(name);
+        System.out.println(email);
+        System.out.println(age);
+
+        return name + " " + email + " " + age;
+    }
+
+    @GetMapping(path = "query-param03")
+    public String queryParam03(UserRequest userRequest){
+
+        System.out.println(userRequest.getName());
+        System.out.println(userRequest.getEmail());
+        System.out.println(userRequest.getAge());
+
+        return userRequest.toString();
+    }
 }
